3164564c6554279bb7d139dd9f78f5958295e3cd
Nick Mangos <nick.mangos@gmail.com>
2016-05-27 13:28:45 +1200

    Initial Commit
    Let’s kick things off.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', function(){
+	console.log('Louise is a mushroom princess!');
+});
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
